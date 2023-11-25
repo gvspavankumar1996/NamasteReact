d7c192ae21562c7cb769d1a0269b15253bbe1522
gvspavan1996 <gvspavankumar1996@gmail.com>
2023-11-25 15:52:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// const heading=React.createElement('h1',{id:"heading"},"heading")
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(heading)
+
+//create nested structure
+
+const parent = React.createElement('div', { id: 'parent' }, [
+  React.createElement('div', { id: 'child1', key: 1 }, [
+    React.createElement('h1', { key: 1 }, 'hello world'),
+    React.createElement('h2', { key: 2 }, 'hello world'),
+  ]),
+  React.createElement('div', { id: 'child2', key: 2 }, [
+    React.createElement('h1', { key: 1 }, 'hello world'),
+    React.createElement('h2', { key: 2 }, 'hello world'),
+  ]),
+]);
+console.log(parent, 'parent');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(parent);

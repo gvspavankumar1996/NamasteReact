5a517dc5c84abeddbf32a29dbf22a58ae834d483
gvspavan1996 <gvspavankumar1996@gmail.com>
2023-11-26 11:31:41 +0530

    episode 2 code

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namste React
+
+...
+# Parcel
+- Dev build
+- local server
+- Hot module replacement(refresh page on each code change)
+- File watching algorithm is used for Hot module replacement which is written in c++
+- caching - faster builds
+- Image optimizaton
+- minification
+- bundling
+- compressing
+- consistent Hashing
+- code splitting
+- differential bundling - support old browsers
+- allow to use HTTPs in local
+- Tree shaking - parcel will remove unused code
+- different dev and prod builds
+
--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 // const heading=React.createElement('h1',{id:"heading"},"heading")
 // const root = ReactDOM.createRoot(document.getElementById('root'))
 // root.render(heading)
@@ -6,7 +9,7 @@
 
 const parent = React.createElement('div', { id: 'parent' }, [
   React.createElement('div', { id: 'child1', key: 1 }, [
-    React.createElement('h1', { key: 1 }, 'hello world'),
+    React.createElement('h1', { key: 1 }, 'hello world 111'),
     React.createElement('h2', { key: 2 }, 'hello world'),
   ]),
   React.createElement('div', { id: 'child2', key: 2 }, [

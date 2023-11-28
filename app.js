d586d12f906ae42bbb6b20747542f04b18efc540
gvspavan1996 <gvspavankumar1996@gmail.com>
2023-11-28 12:36:02 +0530

    Episode 3

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-// const heading=React.createElement('h1',{id:"heading"},"heading")
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(heading)
+// React Functional Component
+const HeadingComponent = () => {
+  return (
+    <div id='container'>
+      <h1 className='heaidng'>Namaste React Fucntional Component</h1>
+    </div>
+  );
+};
 
-//create nested structure
-
-const parent = React.createElement('div', { id: 'parent' }, [
-  React.createElement('div', { id: 'child1', key: 1 }, [
-    React.createElement('h1', { key: 1 }, 'hello world 111'),
-    React.createElement('h2', { key: 2 }, 'hello world'),
-  ]),
-  React.createElement('div', { id: 'child2', key: 2 }, [
-    React.createElement('h1', { key: 1 }, 'hello world'),
-    React.createElement('h2', { key: 2 }, 'hello world'),
-  ]),
-]);
-console.log(parent, 'parent');
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(parent);
+root.render(<HeadingComponent/>);

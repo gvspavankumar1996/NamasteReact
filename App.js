ba9a3cb846d622baeffd5fb66c5e82f30e4de0bf
gvspavan1996 <gvspavankumar1996@gmail.com>
2023-11-28 12:50:19 +0530

    create layout

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const AppLayout = () => {
+  return <div>pavvi</div>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppLayout />);

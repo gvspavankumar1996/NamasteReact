ac82a274d00127605c3b4514989392c53d6768b7
gvspavan1996 <gvspavankumar1996@gmail.com>
2023-12-14 09:37:09 +0530

    episode 6 changes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,37 @@
-import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import RestaurantCard from './RestaurantCard';
+import { useState, useEffect } from 'react';
+import resList from '../utils/mockData';
 
 const Body = () => {
-  let [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    // Optional Chaining
+    setListOfRestaurants(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+
   return (
-    <div className="body">
-      <div className="filter">
+    <div className='body'>
+      <div className='filter'>
         <button
-          className="filter-btn"
+          className='filter-btn'
           onClick={() => {
             const filteredList = (listOfRestaurants = listOfRestaurants.filter(
               (restaurant) => restaurant.info.avgRating > 4
@@ -19,7 +42,7 @@ const Body = () => {
           Top Rated Restaurant in your City
         </button>
       </div>
-      <div className="res-container">
+      <div className='res-container'>
         {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
+import { useState,memo } from 'react';
+// import { useState, useEffect } from 'react';
 import { LOGO_URL } from '../utils/constants';
 
 const Header = () => {
+  const [btnName, setBtnName] = useState('Login');
+  console.log('Header rendered');
   return (
     <div className='header'>
       <div className='logo-container'>
-        <img
-          className='logo'
-          src={LOGO_URL}
-        />
+        <img className='logo' src={LOGO_URL} />
       </div>
       <div className='nav-items'>
         <ul>
@@ -15,10 +16,18 @@ const Header = () => {
           <li>About us</li>
           <li>Contact</li>
           <li>Cart</li>
+          <button
+            className='login-btn'
+            onClick={() => {
+              setBtnName('Logout');
+            }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
   );
 };
 
-export default Header;
+export default memo(Header);
